ee15e39a86165bf2fc061b0825eb2db9cdc6156c
Voksiv <voksiv@gmail.com>
2016-04-14 02:07:00 +0300

    cosmetic

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Php dns-zone-tokenizer
 
+[![Latest Stable Version](https://poser.pugx.org/ltd-beget/dns-zone-tokenizer/version)](https://packagist.org/packages/ltd-beget/dns-zone-tokenizer) 
+[![Total Downloads](https://poser.pugx.org/ltd-beget/dns-zone-tokenizer/downloads)](https://packagist.org/packages/ltd-beget/dns-zone-tokenizer)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/LTD-Beget/dns-zone-tokenizer/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/LTD-Beget/dns-zone-tokenizer/?branch=master)
+[![Code Coverage](https://scrutinizer-ci.com/g/LTD-Beget/dns-zone-tokenizer/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/LTD-Beget/dns-zone-tokenizer/?branch=master)
+[![Build Status](https://scrutinizer-ci.com/g/LTD-Beget/dns-zone-tokenizer/badges/build.png?b=master)](https://scrutinizer-ci.com/g/LTD-Beget/dns-zone-tokenizer/build-status/master)
+[![Documentation](https://img.shields.io/badge/code-documented-brightgreen.svg)](http://ltd-beget.github.io/dns-zone-tokenizer/documentation/html/index.html)
+[![Documentation](https://img.shields.io/badge/code-coverage-brightgreen.svg)](http://ltd-beget.github.io/dns-zone-tokenizer/coverage/index.html)
+[![License MIT](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](https://github.com/LTD-Beget/dns-zone-tokenizer/blob/master/LICENSE)
+
+
+
 Tokenize dns zone files and that's all, folks.
 
 ## Installation
@@ -15,8 +26,8 @@ composer require ltd-beget/dns-zone-tokenizer
     
     require(__DIR__ . '/vendor/autoload.php');
     
-    $config_path = realpath(__DIR__."/zone/exampleZone"); // path to your sphinx conf
-    $plain_config = file_get_contents($config_path); // or some string with sphinx conf
+    $config_path = realpath(__DIR__."/zone/exampleZone"); // path to your dns zone file
+    $plain_config = file_get_contents($config_path);
     
     $tokenized = Tokenizer::tokenize($plain_config); // that's all, folks. All is done =)
 
--- src/LTDBeget/dns/SyntaxErrorException.php
+++ src/LTDBeget/dns/SyntaxErrorException.php
@@ -5,9 +5,10 @@
  * @time: 1:34 PM
  */
 
-
 namespace LTDBeget\dns;
 
+use Exception;
+use LTDBeget\stringstream\StringStream;
 
 /**
  * Class SyntaxErrorException
@@ -15,5 +16,88 @@ namespace LTDBeget\dns;
  */
 class SyntaxErrorException extends \RuntimeException
 {
+//    /**
+//     * SyntaxErrorException constructor.
+//     * @param StringStream $stream
+//     * @param int $code
+//     * @param Exception|null $previous
+//     */
+//    public function __construct(StringStream $stream, int $code = 0, Exception $previous = null)
+//    {
+//        if($stream->isEnd()) {
+//            $stream->end();
+//        }
+//
+//        $this->unexpected_char = $stream->current();
+//        $this->error_line      = $this->getParseErrorLineNumber($stream);
+//        $message               = sprintf($this->messageTemplate, $this->unexpected_char, $this->error_line);
+//        parent::__construct($message, $code, $previous);
+//    }
 
+    /**
+     * @return int
+     */
+    public function getErrorLine()
+    {
+        return $this->error_line;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUnexpectedChar()
+    {
+        return $this->unexpected_char;
+    }
+
+    /**
+     * @internal
+     * @param StringStream $stream
+     * @return int
+     */
+    private function getParseErrorLineNumber(StringStream $stream) : int
+    {
+        $parse_error_char_position = $stream->position();
+        $plain_data                = $this->getFullString($stream);
+        $exploded_by_lines         = explode("\n", $plain_data);
+        foreach ($exploded_by_lines as $key => $line) {
+            $line_length = strlen($line) + 1;
+            $parse_error_char_position -= $line_length;
+            if ($parse_error_char_position < 0) {
+                return $key + 1;
+            }
+        }
+
+        return 1;
+    }
+
+    /**
+     * @param StringStream $stream
+     * @return string
+     */
+    private function getFullString(StringStream $stream) : string
+    {
+        $stream->start();
+        $string = '';
+        do {
+            $string .= $stream->current();
+            $stream->next();
+        } while (! $stream->isEnd());
+
+        return $string;
+    }
+
+    /**
+     * @var int
+     */
+    private $error_line;
+    /**
+     * @var string
+     */
+    private $unexpected_char;
+
+    /**
+     * @var string
+     */
+    private $messageTemplate = "Parse error: syntax error, unexpected '%s' on line %d.";
 }
